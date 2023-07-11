99053bb4a4301518d7f7525ef7aca47633c047b4
Paul Lu <sininglu@sandiego.edu>
2023-07-11 11:38:58 +0800

    added publish/unpublish

--- src/resolvers/mutation/Post.ts
+++ src/resolvers/mutation/Post.ts
@@ -106,4 +106,70 @@ export const PostMutation = {
       };
     }
   },
+
+  publishPost: async (_: any, args: PostArgs, { prisma, auth }: Context) => {
+    const { id: postId } = args;
+
+    // Step 1: check if user is logged in
+    const payload = await getUserFromToken(auth);
+    if (!payload) return { error: { message: "You need to log in first." } };
+    const { userId } = payload;
+
+    // Step 2: check if user can mutate post
+    const canMutate = await canUserMutatePost(prisma, userId, postId);
+    if (canMutate?.error) return canMutate;
+
+    try {
+      const post = await prisma.post.update({
+        where: {
+          id: Number(postId),
+        },
+        data: {
+          published: true,
+        },
+      });
+      return {
+        post,
+      };
+    } catch (e) {
+      return {
+        error: {
+          message: "Prisma Error Code: " + e.code,
+        },
+      };
+    }
+  },
+
+  unpublishPost: async (_: any, args: PostArgs, { prisma, auth }: Context) => {
+    const { id: postId } = args;
+
+    // Step 1: check if user is logged in
+    const payload = await getUserFromToken(auth);
+    if (!payload) return { error: { message: "You need to log in first." } };
+    const { userId } = payload;
+
+    // Step 2: check if user can mutate post
+    const canMutate = await canUserMutatePost(prisma, userId, postId);
+    if (canMutate?.error) return canMutate;
+
+    try {
+      const post = await prisma.post.update({
+        where: {
+          id: Number(postId),
+        },
+        data: {
+          published: false,
+        },
+      });
+      return {
+        post,
+      };
+    } catch (e) {
+      return {
+        error: {
+          message: "Prisma Error Code: " + e.code,
+        },
+      };
+    }
+  },
 };
